50962d2686831a5610b6647dc80bbcb9fc324d25
Md Jamaluddin <mdjamaluddina007@gmail.com>
2025-01-08 14:41:11 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import Education from './components/Education';
-import Skills from './components/skills';
+import Skills from './components/Skills'
 import Contact from './components/Contact';
 
 
